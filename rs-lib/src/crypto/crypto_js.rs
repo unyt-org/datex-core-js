2455390b53a0958acef1ea4dd8136e181a8ecb24
Jonas Strehle <jonas@strehles.info>
2025-03-12 21:40:55 +0100

    Add support for js crypto

--- rs-lib/src/crypto/crypto_js.rs
+++ rs-lib/src/crypto/crypto_js.rs
@@ -6,6 +6,12 @@ extern "C" {
     fn randomUUID() -> String;
 }
 pub struct CryptoJS;
+impl CryptoJS {
+    fn crypto() -> web_sys::Crypto {
+        let window_instance = web_sys::window().unwrap();
+        window_instance.crypto().unwrap()
+    }
+}
 impl Crypto for CryptoJS {
     fn encrypt(&self, data: &[u8]) -> Vec<u8> {
         todo!()
@@ -16,10 +22,15 @@ impl Crypto for CryptoJS {
     }
 
     fn create_uuid(&self) -> String {
-        return randomUUID();
+        randomUUID()
+        //return Self::crypto().randomUUID();
     }
 
     fn random_bytes(&self, length: usize) -> Vec<u8> {
-        todo!()
+        let mut buffer = &mut vec![0u8; length];
+        Self::crypto()
+            .get_random_values_with_u8_array(&mut buffer)
+            .unwrap();
+        buffer.to_vec()
     }
 }
