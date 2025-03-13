2047f61b7dd6c6443fb56be48a1cfee9601e2e3b
Jonas Strehle <jonas@strehles.info>
2025-03-13 18:19:44 +0100

    Work on js crypto keys

--- rs-lib/src/crypto/crypto_js.rs
+++ rs-lib/src/crypto/crypto_js.rs
@@ -1,16 +1,84 @@
-use datex_core::crypto::crypto::Crypto;
+use std::array;
+
+use datex_core::crypto::{
+    self,
+    crypto::{Crypto, CryptoError},
+};
+use wasm_bindgen::JsValue;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{
+    js_sys::{self, Array, Object},
+    CryptoKey, EcdsaParams, RsaOaepParams,
+};
+
+fn js_array(values: &[&str]) -> JsValue {
+    return JsValue::from(
+        values
+            .iter()
+            .map(|x| JsValue::from_str(x))
+            .collect::<js_sys::Array>(),
+    );
+}
 
 pub struct CryptoJS;
 impl CryptoJS {
     fn window() -> web_sys::Window {
-        web_sys::window().unwrap()
+        web_sys::window().expect("no global window exists.")
     }
     fn crypto() -> web_sys::Crypto {
-        Self::window().crypto().unwrap()
+        Self::window().crypto().expect("no global crypto exists.")
+    }
+    fn crypto_subtle() -> web_sys::SubtleCrypto {
+        Self::crypto().subtle()
+    }
+    fn generate_key_pair(&self) {}
+
+    async fn generate_key(
+        &self,
+        algorithm: &Object,
+        extractable: bool,
+        key_usages: &[&str],
+    ) -> Result<CryptoKey, CryptoError> {
+        let key_generator_promise = Self::crypto_subtle()
+            .generate_key_with_object(
+                &algorithm,
+                extractable,
+                &js_array(&key_usages),
+            )
+            .map_err(|_| CryptoError::KeyGeneratorFailed)?;
+        let key: JsValue = JsFuture::from(key_generator_promise)
+            .await
+            .map_err(|_| CryptoError::KeyGeneratorFailed)?;
+        let crypto_key: CryptoKey = key
+            .try_into()
+            .map_err(|_| CryptoError::KeyGeneratorFailed)?;
+        Ok(crypto_key)
+    }
+
+    pub async fn new_encryption_key(&self) -> Result<CryptoKey, CryptoError> {
+        self.generate_key(
+            &RsaOaepParams::new("RSA-OAEP"),
+            true,
+            &["encrypt", "decrypt"],
+        )
+        .await
+    }
+    pub async fn new_sign_key(&self) -> Result<CryptoKey, CryptoError> {
+        self.generate_key(
+            &EcdsaParams::new("RSA-OAEP", &JsValue::from_str("SHA-256")),
+            true,
+            &["sign", "verify"],
+        )
+        .await
     }
 }
 impl Crypto for CryptoJS {
     fn encrypt(&self, data: &[u8]) -> Vec<u8> {
+        // let crypto_key = Self::crypto_subtle().generate_key_with_str(
+        //     "AES-GCM", true, &["encrypt", "decrypt"]
+        // ).unwrap();
+        // Self::crypto_subtle().encrypt_with_str_and_u8_array(
+        //     "xx", key, data)
         todo!()
     }
 
