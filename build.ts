64859a9870555cc67315508f99f581da6ddc9926
Jonas Strehle <jonas@strehles.info>
2025-03-20 17:33:06 +0100

    Fix build inline issue

--- build.ts
+++ build.ts
@@ -1,23 +1,34 @@
-import * as build from "https://jsr.io/@deno/wasmbuild/0.19.1/lib/commands/build_command.ts";
+import { runBuildCommand } from "https://jsr.io/@deno/wasmbuild/0.19.1/lib/commands/build_command.ts";
 import { Path } from "jsr:@david/path@^0.2.0";
+import { format } from "https://deno.land/std@0.224.0/fmt/bytes.ts";
 
-await build.runBuildCommand({
-    profile: "release",
-    kind: "build",
-    inline: false,
-    outDir: new Path("./src/datex-core"),
-    bindingJsFileExt: "js",
-    isOpt: true, // set false to skip wasm-opt
-    project: "datex-core-js",
-    cargoFlags: ["--no-default-features"],
-});
+const NAME = "datex_core_js";
 
-const jsFile = `
-import * as imports from "./datex_core_js.internal.js";
-const wasm = (await WebAssembly.instantiateStreaming(fetch(new URL("datex_core_js.wasm", import.meta.url)), {
-    "./datex_core_js.internal.js": imports,
+const outDir = new Path("./src/datex-core");
+try {
+    await runBuildCommand({
+        isOpt: true, // set false to skip wasm-opt
+        outDir,
+        profile: "release",
+        kind: "build",
+        inline: false,
+        bindingJsFileExt: "js",
+        project: "datex-core-js",
+        cargoFlags: ["--no-default-features"],
+    });
+} catch (e) {
+    console.error(`❌ Build failed:`, e);
+    Deno.exit(1);
+}
+
+const jsFile = `import * as imports from "./${NAME}.internal.js";
+const wasm = (await WebAssembly.instantiateStreaming(fetch(new URL("${NAME}.wasm", import.meta.url)), {
+    "./${NAME}.internal.js": imports,
 })).instance;
-export * from "./datex_core_js.internal.js";
-import { __wbg_set_wasm } from "./datex_core_js.internal.js";
-__wbg_set_wasm(wasm.exports);
-`;
+export * from "./${NAME}.internal.js";
+import { __wbg_set_wasm } from "./${NAME}.internal.js";
+__wbg_set_wasm(wasm.exports);`;
+
+await outDir.resolve(`${NAME}.js`).writeText(jsFile);
+const fileSize = (await outDir.resolve(`${NAME}.wasm`).stat())!.size;
+console.info(`✅ Build complete: (${format(fileSize)})`);
--- src/datex-core/datex_core_js.js
+++ src/datex-core/datex_core_js.js
@@ -1,10 +1,3 @@
-// @generated file from wasmbuild -- do not edit
-// @ts-nocheck: generated
-// deno-lint-ignore-file
-// deno-fmt-ignore-file
-// @ts-self-types="./datex_core_js.d.ts"
-
-// source-hash: 8a980cfa016dfa0bc621f23160ef0d9ede111ccf
 import * as imports from "./datex_core_js.internal.js";
 const wasm = (await WebAssembly.instantiateStreaming(fetch(new URL("datex_core_js.wasm", import.meta.url)), {
     "./datex_core_js.internal.js": imports,
