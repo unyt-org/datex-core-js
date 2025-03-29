f7896c9ca571694d194f0cbab22d5fc68875c946
Benedikt Strehle <benedikt.strehle@uni-ulm.de>
2025-03-29 13:11:34 +0100

    update build-wasm.ts to fix formatting

--- scripts/build-wasm.ts
+++ scripts/build-wasm.ts
@@ -1,8 +1,9 @@
-import { runBuildCommand } from "https://jsr.io/@deno/wasmbuild/0.19.1/lib/commands/build_command.ts";
-import { Path } from "jsr:@david/path@^0.2.0";
-import { format } from "https://deno.land/std@0.224.0/fmt/bytes.ts";
+import {runBuildCommand} from "https://jsr.io/@deno/wasmbuild/0.19.1/lib/commands/build_command.ts";
+import {Path} from "jsr:@david/path@^0.2.0";
+import {format} from "https://deno.land/std@0.224.0/fmt/bytes.ts";
 
-import { parseArgs } from "jsr:@std/cli/parse-args";
+import {parseArgs} from "jsr:@std/cli/parse-args";
+
 const flags = parseArgs(Deno.args, {
     boolean: ["opt"],
     string: ["profile"],
@@ -29,12 +30,16 @@ try {
 }
 
 const jsFile = `import * as imports from "./${NAME}.internal.js";
-const wasm = (await WebAssembly.instantiateStreaming(fetch(new URL("${NAME}.wasm", import.meta.url)), {
-    "./${NAME}.internal.js": imports,
-})).instance;
+const wasm = (await WebAssembly.instantiateStreaming(
+    fetch(new URL("${NAME}.wasm", import.meta.url)),
+    {
+        "./${NAME}.internal.js": imports,
+    },
+)).instance;
 export * from "./${NAME}.internal.js";
 import { __wbg_set_wasm } from "./${NAME}.internal.js";
-__wbg_set_wasm(wasm.exports);`;
+__wbg_set_wasm(wasm.exports);
+`;
 
 await outDir.resolve(`${NAME}.js`).writeText(jsFile);
 const fileSize = (await outDir.resolve(`${NAME}.wasm`).stat())!.size;
