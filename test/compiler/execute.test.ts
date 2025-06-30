4bb9a73aeac31c7aa7901c17c4186c0f7b826851
Benedikt Strehle <benedikt@unyt.org>
2025-06-30 23:50:42 +0200

    update tests

--- test/compiler/execute.test.ts
+++ test/compiler/execute.test.ts
@@ -1,8 +1,9 @@
 import { Runtime } from "../../src/runtime/runtime.ts";
-
-Deno.test("execute", async () => {
+import { assertEquals } from "jsr:@std/assert";
+Deno.test("execute", () => {
     const runtime = new Runtime("@jonas");
     const script = "1 + 2";
     const result = runtime.execute(script, true);
+    assertEquals(result, "3");
     console.log(result);
 });
