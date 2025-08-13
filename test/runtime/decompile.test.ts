832a8b4e23fefd65c88da83ae1a27ce0bafee52b
Benedikt Strehle <benedikt@unyt.org>
2025-08-14 00:31:36 +0200

    deno fmt

--- test/runtime/decompile.test.ts
+++ test/runtime/decompile.test.ts
@@ -4,10 +4,13 @@ Deno.test("decompile integer without formatting", () => {
     const runtime = new Runtime({ endpoint: "@jonas" });
     const result = runtime.valueToString(42);
     assertEquals(result, "42.0");
-})
+});
 
 Deno.test("decompile integer colorized", () => {
     const runtime = new Runtime({ endpoint: "@jonas" });
     const result = runtime.valueToString(42, { colorized: true });
-    assertEquals(result, "\x1B[38;2;231;139;71m42\x1B[38;2;212;212;212m.\x1B[38;2;231;139;71m0\x1B[0m");
-})
+    assertEquals(
+        result,
+        "\x1B[38;2;231;139;71m42\x1B[38;2;212;212;212m.\x1B[38;2;231;139;71m0\x1B[0m",
+    );
+});
