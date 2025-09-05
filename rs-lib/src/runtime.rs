0be9c6789ca46802c69ef2d8063caa8d2c321fcd
Jonas Strehle <jonas@strehles.info>
2025-09-05 19:00:35 +0200

    fix to new api

--- rs-lib/src/runtime.rs
+++ rs-lib/src/runtime.rs
@@ -1,36 +1,36 @@
-use datex_core::values::core_values::endpoint::Endpoint;
-#[cfg(feature = "debug")]
-use datex_core::runtime::global_context::DebugFlags;
-use datex_core::runtime::global_context::GlobalContext;
-use serde::{Deserialize, Serialize};
-use std::str::FromStr;
-use std::sync::{Arc, Mutex};
-use log::info;
 use crate::crypto::crypto_js::CryptoJS;
 use crate::js_utils::js_array;
+use crate::memory::JSMemory;
+use crate::network::com_hub::JSComHub;
 use crate::utils::time::TimeJS;
 use datex_core::crypto::crypto::CryptoTrait;
-use datex_core::decompiler::{decompile_value, DecompileOptions};
+use datex_core::decompiler::{DecompileOptions, decompile_value};
 use datex_core::dif::DIFValue;
 use datex_core::global::dxb_block::DXBBlock;
 use datex_core::global::protocol_structures::block_header::{
     BlockHeader, FlagsAndTimestamp,
 };
+#[cfg(feature = "debug")]
+use datex_core::runtime::global_context::DebugFlags;
+use datex_core::runtime::global_context::GlobalContext;
 use datex_core::runtime::{Runtime, RuntimeConfig, RuntimeInternal};
+use datex_core::values::core_values::endpoint::Endpoint;
 use datex_core::values::serde::deserializer::DatexDeserializer;
 use datex_core::values::value_container::ValueContainer;
+use log::info;
+use serde::{Deserialize, Serialize};
 use serde_wasm_bindgen::from_value;
+use std::str::FromStr;
+use std::sync::{Arc, Mutex};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::future_to_promise;
 use web_sys::js_sys::Promise;
-use crate::memory::JSMemory;
-use crate::network::com_hub::JSComHub;
 
 #[wasm_bindgen(getter_with_clone)]
 pub struct JSRuntime {
     runtime: Runtime,
     pub com_hub: JSComHub,
-    pub memory: JSMemory
+    pub memory: JSMemory,
 }
 
 #[derive(Serialize, Deserialize, Default)]
@@ -63,7 +63,6 @@ struct JSDecompileOptions {
  * Internal impl of the JSRuntime, not exposed to JavaScript
  */
 impl JSRuntime {
-
     pub fn runtime(&self) -> &Runtime {
         &self.runtime
     }
@@ -73,7 +72,8 @@ impl JSRuntime {
         debug_flags: Option<JSDebugFlags>,
     ) -> JSRuntime {
         let deserializer = DatexDeserializer::from_script(config).unwrap();
-        let config: RuntimeConfig = Deserialize::deserialize(deserializer).unwrap();
+        let config: RuntimeConfig =
+            Deserialize::deserialize(deserializer).unwrap();
         let runtime = Runtime::init(
             config,
             GlobalContext {
@@ -96,14 +96,13 @@ impl JSRuntime {
         runtime
     }
 
-
     pub fn new(runtime: Runtime) -> JSRuntime {
         let com_hub = JSComHub::new(runtime.clone());
         let memory = JSMemory::new(runtime.clone());
         JSRuntime {
             runtime,
             com_hub,
-            memory
+            memory,
         }
     }
 }
@@ -113,7 +112,6 @@ impl JSRuntime {
  */
 #[wasm_bindgen]
 impl JSRuntime {
-
     pub async fn crypto_test_tmp(&self) -> Promise {
         future_to_promise(async move {
             let crypto = CryptoJS {};
@@ -231,21 +229,21 @@ impl JSRuntime {
         dif_values: Option<Vec<JsValue>>,
         decompile_options: JsValue,
     ) -> String {
-        let result = self.runtime.execute(
-            script,
-            &Self::js_values_to_value_containers(dif_values),
-            None
-        ).await.unwrap();
+        let result = self
+            .runtime
+            .execute(
+                script,
+                &Self::js_values_to_value_containers(dif_values),
+                None,
+            )
+            .await
+            .unwrap();
         match result {
-            None => {
-                "".to_string()
-            }
-            Some(result) => {
-                decompile_value(
-                    &result,
-                    Self::decompile_options_from_js_value(decompile_options)
-                )
-            }
+            None => "".to_string(),
+            Some(result) => decompile_value(
+                &result,
+                Self::decompile_options_from_js_value(decompile_options),
+            ),
         }
     }
 
@@ -254,11 +252,15 @@ impl JSRuntime {
         script: &str,
         dif_values: Option<Vec<JsValue>>,
     ) -> JsValue {
-        let result = self.runtime.execute(
-            script,
-            &Self::js_values_to_value_containers(dif_values),
-            None
-        ).await.unwrap();
+        let result = self
+            .runtime
+            .execute(
+                script,
+                &Self::js_values_to_value_containers(dif_values),
+                None,
+            )
+            .await
+            .unwrap();
         Self::maybe_value_container_to_dif(result)
     }
 
@@ -268,21 +270,20 @@ impl JSRuntime {
         dif_values: Option<Vec<JsValue>>,
         decompile_options: JsValue,
     ) -> String {
-        let input = self.runtime.execute_sync(
-            script,
-            &Self::js_values_to_value_containers(dif_values),
-            None
-        ).unwrap();
+        let input = self
+            .runtime
+            .execute_sync(
+                script,
+                &Self::js_values_to_value_containers(dif_values),
+                None,
+            )
+            .unwrap();
         match input {
-            None => {
-                "".to_string()
-            }
-            Some(result) => {
-                decompile_value(
-                    &result,
-                    Self::decompile_options_from_js_value(decompile_options)
-                )
-            }
+            None => "".to_string(),
+            Some(result) => decompile_value(
+                &result,
+                Self::decompile_options_from_js_value(decompile_options),
+            ),
         }
     }
 
@@ -291,11 +292,14 @@ impl JSRuntime {
         script: &str,
         dif_values: Option<Vec<JsValue>>,
     ) -> JsValue {
-        let result = self.runtime.execute_sync(
-            script,
-            &Self::js_values_to_value_containers(dif_values),
-            None
-        ).unwrap();
+        let result = self
+            .runtime
+            .execute_sync(
+                script,
+                &Self::js_values_to_value_containers(dif_values),
+                None,
+            )
+            .unwrap();
         Self::maybe_value_container_to_dif(result)
     }
 
@@ -307,18 +311,15 @@ impl JSRuntime {
             Self::js_value_to_value_container(dif_value);
         decompile_value(
             &value_container,
-            Self::decompile_options_from_js_value(decompile_options)
+            Self::decompile_options_from_js_value(decompile_options),
         )
     }
 
     fn maybe_value_container_to_dif(
-        maybe_value_container: Option<ValueContainer>
+        maybe_value_container: Option<ValueContainer>,
     ) -> JsValue {
-
         match maybe_value_container {
-            None => {
-                JsValue::NULL
-            }
+            None => JsValue::NULL,
             Some(value_container) => {
                 let dif_value = DIFValue::from(&value_container);
                 serde_wasm_bindgen::to_value(&dif_value).unwrap()
@@ -329,25 +330,26 @@ impl JSRuntime {
     fn js_values_to_value_containers(
         js_values: Option<Vec<JsValue>>,
     ) -> Vec<ValueContainer> {
-        js_values.map(|values| {
-            values
-                .into_iter()
-                .map(|v| Self::js_value_to_value_container(v))
-                .collect()
-        }).unwrap_or_default()
+        js_values
+            .map(|values| {
+                values
+                    .into_iter()
+                    .map(|v| Self::js_value_to_value_container(v))
+                    .collect()
+            })
+            .unwrap_or_default()
     }
 
-    fn js_value_to_value_container(
-        js_value: JsValue,
-    ) -> ValueContainer {
+    fn js_value_to_value_container(js_value: JsValue) -> ValueContainer {
         // convert JsValue to DIFValue
-        let dif_value: DIFValue = serde_wasm_bindgen::from_value(js_value).unwrap();
+        let dif_value: DIFValue =
+            serde_wasm_bindgen::from_value(js_value).unwrap();
         // convert DIFValue to ValueContainer
         ValueContainer::from(&dif_value)
     }
 
     fn decompile_options_from_js_value(
-        decompile_options: JsValue
+        decompile_options: JsValue,
     ) -> DecompileOptions {
         // if null, return default options
         if decompile_options.is_null() {
@@ -355,11 +357,14 @@ impl JSRuntime {
         }
         // if not null, try to deserialize
         else {
-            let js_decompile_options: JSDecompileOptions = from_value(decompile_options).unwrap_or_default();
+            let js_decompile_options: JSDecompileOptions =
+                from_value(decompile_options).unwrap_or_default();
             DecompileOptions {
-                formatted: js_decompile_options.formatted.unwrap_or(false),
+                formatting: Default::default(),
                 colorized: js_decompile_options.colorized.unwrap_or(false),
-                resolve_slots: js_decompile_options.resolve_slots.unwrap_or(false),
+                resolve_slots: js_decompile_options
+                    .resolve_slots
+                    .unwrap_or(false),
                 json_compat: js_decompile_options.json_compat.unwrap_or(false),
             }
         }
