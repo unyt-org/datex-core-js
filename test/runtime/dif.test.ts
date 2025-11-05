c0e1b6e0dc6b6d25036d303a14f1bb75f57d9efe
Benedikt Strehle <benedikt@unyt.org>
2025-11-05 23:29:09 +0100

    fixes

--- test/runtime/dif.test.ts
+++ test/runtime/dif.test.ts
@@ -2,7 +2,6 @@ import { Runtime } from "../../src/runtime/runtime.ts";
 import { assert, assertEquals } from "@std/assert";
 import { assertThrows } from "@std/assert/throws";
 import {
-    CoreTypeAddress,
     type DIFReference,
     DIFReferenceMutability,
     type DIFRepresentationValue,
@@ -10,6 +9,7 @@ import {
     type DIFUpdateData,
     DIFUpdateKind,
 } from "../../src/dif/definitions.ts";
+import { CoreTypeAddress } from "../../src/dif/core.ts";
 import { assertStrictEquals } from "@std/assert/strict-equals";
 import { Ref } from "../../src/refs/ref.ts";
 import {
