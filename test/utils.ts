62898d85937781856713419443dc10f824847046
Jonas Strehle <jonas@strehles.info>
2025-11-10 19:21:55 +0100

    add comments

--- test/utils.ts
+++ test/utils.ts
@@ -1,6 +1,16 @@
+/**
+ * Sleeps for the given number of milliseconds.
+ * @param ms Number of milliseconds to sleep
+ * @returns A promise that resolves after the specified time
+ */
 export function sleep(ms: number) {
     return new Promise<void>((resolve) => setTimeout(resolve, ms));
 }
+
+/**
+ * Waits for the next event loop tick.
+ * @returns A promise that resolves on the next event loop tick
+ */
 export function nextTick() {
     return sleep(0);
 }
