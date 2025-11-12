a96cafdc2be461224fde674122c790ddf702fa89
Jonas Strehle <jonas@strehles.info>
2025-11-12 20:30:22 +0100

    add lsp support

---
+++ test/lsp/BasicConnect.test.ts
@@ -0,0 +1,29 @@
+import { dedent } from "@qnighy/dedent";
+import { Runtime } from "../../src/runtime/runtime.ts";
+import { sleep } from "../utils.ts";
+import { assertEquals } from "@std/assert/equals";
+
+Deno.test("basic lsp", async () => {
+    const INIT_FRAME = dedent`{
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+            "capabilities": {},
+            "rootUri": null,
+            "workspaceFolders": null
+        }
+    }`;
+
+    const runtime = new Runtime({ endpoint: "@unyt", debug: true });
+    const queue = [];
+    const send = runtime.startLSP(
+        (data: string) => {
+            console.log("recv:", data);
+            queue.push(data);
+        },
+    );
+    send(`Content-Length: ${INIT_FRAME.length}\r\n\r\n${INIT_FRAME}`);
+    await sleep(100);
+    assertEquals(queue.length, 1, "Should have one message in queue");
+});
