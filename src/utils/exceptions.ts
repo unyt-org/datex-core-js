c25408700447b2be97e10117126d77baade0ad84
Benedikt Strehle <benedikt@unyt.org>
2025-11-18 00:45:06 +0100

    work on invalidation for moved array values

--- src/utils/exceptions.ts
+++ src/utils/exceptions.ts
@@ -1,11 +1,11 @@
-export function panic(message: string): never {
+export function panic(message: string = "unknown"): never {
     throw new Error(`Panic: ${message}`);
 }
 
-export function unreachable(message: string): never {
+export function unreachable(message: string = "unknown"): never {
     throw new Error(`Unreachable code reached: ${message}`);
 }
 
-export function unimplemented(message: string): never {
+export function unimplemented(message: string = "unknown"): never {
     throw new Error(`Not implemented: ${message}`);
 }
