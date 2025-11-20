40d7342dc8ccc3d1c7f4928ea3cd64e817d0a4aa
Benedikt Strehle <benedikt@unyt.org>
2025-11-20 22:30:51 +0100

    add more array tests and method intercepts

--- test/lib/js-core-types/array.test.ts
+++ test/lib/js-core-types/array.test.ts
@@ -83,6 +83,61 @@ Deno.test("array replace external", () => {
     assertEquals(arrayPtr, ["a", "b", "c"]);
 });
 
+Deno.test("array splice external", () => {
+    // create mutable ref to array
+    const array = ["value1", "value2", 123, "value4"];
+    const [arrayPtr, address] = createArrayReference(array);
+
+    runtime._runtime.dif().update(42, address, {
+        kind: DIFUpdateKind.ListSplice,
+        start: 1,
+        delete_count: 2,
+        items: [
+            runtime.dif.convertJSValueToDIFValueContainer("newValueA"),
+            runtime.dif.convertJSValueToDIFValueContainer("newValueB"),
+        ],
+    });
+    assertEquals(arrayPtr, ["value1", "newValueA", "newValueB", "value4"]);
+
+    runtime._runtime.dif().update(42, address, {
+        kind: DIFUpdateKind.ListSplice,
+        start: 2,
+        delete_count: 2,
+        items: [],
+    });
+    assertEquals(arrayPtr, ["value1", "newValueA"]);
+});
+
+Deno.test("array set local", () => {
+    // create mutable ref to array
+    const array = ["a", "b", "c"];
+    const [arrayPtr, address] = createArrayReference(array);
+
+    arrayPtr[1] = "localValue";
+
+    assertEquals(getCurrentRuntimeLocalValue(address), [
+        "a",
+        "localValue",
+        "c",
+    ]);
+});
+
+Deno.test("array set length local", () => {
+    // create mutable ref to array
+    const array = ["a", "b", "c"];
+    const [arrayPtr, address] = createArrayReference(array);
+
+    arrayPtr.length = 5;
+
+    assertEquals(getCurrentRuntimeLocalValue(address), [
+        "a",
+        "b",
+        "c",
+        null,
+        null,
+    ]);
+});
+
 Deno.test("array push local", () => {
     // create mutable ref to array
     const array = ["a", "b", "c"];
@@ -99,19 +154,169 @@ Deno.test("array push local", () => {
     ]);
 });
 
-Deno.test("array set length local", () => {
+Deno.test("array splice local", () => {
     // create mutable ref to array
-    const array = ["a", "b", "c"];
-    const [arrayPtr, address] = createArrayReference(array);
-    // 2. local update
+    const [arrayPtr, address] = createArrayReference([
+        "value1",
+        "value2",
+        123,
+        "value4",
+    ]);
 
-    arrayPtr.length = 5;
+    arrayPtr.splice(1, 2, "newValueA", "newValueB");
 
     assertEquals(getCurrentRuntimeLocalValue(address), [
-        "a",
-        "b",
-        "c",
-        null,
-        null,
+        "value1",
+        "newValueA",
+        "newValueB",
+        "value4",
+    ]);
+
+    arrayPtr.splice(2, 1);
+
+    assertEquals(getCurrentRuntimeLocalValue(address), [
+        "value1",
+        "newValueA",
+        "value4",
+    ]);
+});
+
+Deno.test("array reverse local", () => {
+    // create mutable ref to array
+    const [arrayPtr, address] = createArrayReference([
+        "value1",
+        "value2",
+        123,
+        "value4",
+    ]);
+
+    arrayPtr.reverse();
+    assertEquals(getCurrentRuntimeLocalValue(address), [
+        "value4",
+        123,
+        "value2",
+        "value1",
+    ]);
+});
+
+Deno.test("array sort local", () => {
+    // create mutable ref to array
+    const [arrayPtr, address] = createArrayReference([
+        "banana",
+        "apple",
+        "cherry",
+    ]);
+
+    arrayPtr.sort();
+    assertEquals(getCurrentRuntimeLocalValue(address), [
+        "apple",
+        "banana",
+        "cherry",
+    ]);
+});
+
+Deno.test("array pop local", () => {
+    // create mutable ref to array
+    const [arrayPtr, address] = createArrayReference([
+        "value1",
+        "value2",
+        123,
+        "value4",
+    ]);
+
+    const popped = arrayPtr.pop();
+    assertEquals(popped, "value4");
+    assertEquals(getCurrentRuntimeLocalValue(address), [
+        "value1",
+        "value2",
+        123,
+    ]);
+});
+
+Deno.test("array shift local", () => {
+    // create mutable ref to array
+    const [arrayPtr, address] = createArrayReference([
+        "value1",
+        "value2",
+        123,
+        "value4",
+    ]);
+
+    const shifted = arrayPtr.shift();
+    assertEquals(shifted, "value1");
+    assertEquals(getCurrentRuntimeLocalValue(address), [
+        "value2",
+        123,
+        "value4",
+    ]);
+});
+
+Deno.test("array unshift local", () => {
+    // create mutable ref to array
+    const [arrayPtr, address] = createArrayReference([
+        "value1",
+        "value2",
+        123,
+        "value4",
+    ]);
+
+    arrayPtr.unshift("newValue1", "newValue2");
+    assertEquals(getCurrentRuntimeLocalValue(address), [
+        "newValue1",
+        "newValue2",
+        "value1",
+        "value2",
+        123,
+        "value4",
+    ]);
+});
+
+Deno.test("array fill local", () => {
+    // create mutable ref to array
+    const [arrayPtr, address] = createArrayReference([
+        "value1",
+        "value2",
+        123,
+        "value4",
+    ]);
+
+    arrayPtr.fill("filledValue", 1, 6);
+    assertEquals(getCurrentRuntimeLocalValue(address), [
+        "value1",
+        "filledValue",
+        "filledValue",
+        "filledValue",
+    ]);
+
+    arrayPtr.fill("allFilled");
+    assertEquals(getCurrentRuntimeLocalValue(address), [
+        "allFilled",
+        "allFilled",
+        "allFilled",
+        "allFilled",
+    ]);
+
+    arrayPtr.fill("noChange", 0, 0);
+    assertEquals(getCurrentRuntimeLocalValue(address), [
+        "allFilled",
+        "allFilled",
+        "allFilled",
+        "allFilled",
+    ]);
+
+    arrayPtr.fill("excludeLast", 0, -1);
+    assertEquals(getCurrentRuntimeLocalValue(address), [
+        "excludeLast",
+        "excludeLast",
+        "excludeLast",
+        "allFilled",
+    ]);
+
+    arrayPtr.fill("excludeFirst", 1);
+    assertEquals(getCurrentRuntimeLocalValue(address), [
+        "excludeLast",
+        "excludeFirst",
+        "excludeFirst",
+        "excludeFirst",
     ]);
 });
