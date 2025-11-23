4a6c0b993ad8950d285986ab9e6732ce181f6cf2
Benedikt Strehle <benedikt@unyt.org>
2025-11-24 00:46:52 +0100

    work on js lib marker types

---
+++ src/dif/js-lib.ts
@@ -0,0 +1,9 @@
+export const JsLibTypeAddress = {
+    undefined: "111111",
+} as const;
+
+/**
+ * Type representing the unique pointer addresses of js lib types.
+ */
+export type JsLibTypeAddress =
+    typeof JsLibTypeAddress[keyof typeof JsLibTypeAddress];
