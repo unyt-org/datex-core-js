0f0ba7c9ab1516e93ec1bd73ae7c1221726de7d2
Jonas Strehle <jonas@strehles.info>
2026-01-18 10:54:29 +0100

    refactor to new API (WIP)

--- rs-lib/src/network/com_hub.rs
+++ rs-lib/src/network/com_hub.rs
@@ -1,13 +1,17 @@
-use datex_core::global::dxb_block::IncomingSection;
-use datex_core::network::com_hub::{ComHubError, InterfacePriority};
-use datex_core::network::com_interfaces::com_interface::{
-    ComInterface, ComInterfaceFactory, ComInterfaceUUID,
+use datex_core::{
+    global::dxb_block::{DXBBlock, IncomingSection},
+    network::{
+        com_hub::{ComHub, InterfacePriority},
+        com_interfaces::com_interface::{
+            ComInterface, ComInterfaceUUID, socket::ComInterfaceSocketUUID,
+        },
+    },
+    runtime::{AsyncContext, Runtime},
+    stdlib::{cell::RefCell, rc::Rc},
+    utils::uuid::UUID,
+    values::core_values::endpoint::Endpoint,
 };
-use datex_core::network::com_interfaces::com_interface_socket::ComInterfaceSocketUUID;
-use datex_core::runtime::{AsyncContext, Runtime};
-use datex_core::stdlib::{cell::RefCell, rc::Rc};
-use datex_core::values::core_values::endpoint::Endpoint;
-use datex_core::{network::com_hub::ComHub, utils::uuid::UUID};
+use js_sys::Uint8Array;
 use log::error;
 use std::str::FromStr;
 use wasm_bindgen::prelude::*;
@@ -29,23 +33,23 @@ impl JSComHub {
         JSComHub { runtime }
     }
 
-    pub fn com_hub(&self) -> &ComHub {
+    pub fn com_hub(&self) -> Rc<ComHub> {
         self.runtime.com_hub()
     }
 
-    pub fn get_interface_for_uuid<T: ComInterface>(
-        &self,
-        uuid: String,
-    ) -> Result<Rc<RefCell<T>>, ComHubError> {
-        let base_interface = self
-            .com_hub()
-            .get_interface_by_uuid::<T>(&ComInterfaceUUID::from_string(uuid));
-        if let Some(base_interface) = base_interface {
-            Ok(base_interface)
-        } else {
-            Err(ComHubError::InterfaceDoesNotExist)
-        }
-    }
+    // pub fn get_interface_for_uuid<T: ComInterface>(
+    //     &self,
+    //     uuid: String,
+    // ) -> Result<Rc<RefCell<T>>, ComHubError> {
+    //     let base_interface = self
+    //         .com_hub()
+    //         .get_interface_by_uuid::<T>(&ComInterfaceUUID::from_string(uuid));
+    //     if let Some(base_interface) = base_interface {
+    //         Ok(base_interface)
+    //     } else {
+    //         Err(ComHubError::InterfaceDoesNotExist)
+    //     }
+    // }
 }
 
 /**
@@ -54,21 +58,21 @@ impl JSComHub {
 #[wasm_bindgen]
 impl JSComHub {
     pub fn register_default_interface_factories(&self) {
-        self.com_hub().register_sync_interface_factory::<crate::network::com_interfaces::base_interface::BaseJSInterface>();
+        // self.com_hub().register_sync_interface_factory::<crate::network::com_interfaces::base_interface::BaseJSInterface>();
 
         #[cfg(feature = "wasm_websocket_client")]
-        self.com_hub().register_sync_interface_factory::<crate::network::com_interfaces::websocket_client_js_interface::WebSocketClientJSInterface>();
+        self.com_hub().register_async_interface_factory::<crate::network::com_interfaces::websocket_client_js_interface::WebSocketClientJSInterfaceSetupData>();
 
-        #[cfg(feature = "wasm_websocket_server")]
-        self.com_hub().register_sync_interface_factory::<crate::network::com_interfaces::websocket_server_js_interface::WebSocketServerJSInterface>();
+        // #[cfg(feature = "wasm_websocket_server")]
+        // self.com_hub().register_sync_interface_factory::<crate::network::com_interfaces::websocket_server_js_interface::WebSocketServerJSInterface>();
 
-        //wasm_serial
-        #[cfg(feature = "wasm_serial")]
-        self.com_hub().register_sync_interface_factory::<crate::network::com_interfaces::serial_js_interface::SerialJSInterface>();
+        // //wasm_serial
+        // #[cfg(feature = "wasm_serial")]
+        // self.com_hub().register_sync_interface_factory::<crate::network::com_interfaces::serial_js_interface::SerialJSInterface>();
 
-        //wasm_webrtc
-        #[cfg(feature = "wasm_webrtc")]
-        self.com_hub().register_sync_interface_factory::<crate::network::com_interfaces::webrtc_js_interface::WebRTCJSInterface>();
+        // //wasm_webrtc
+        // #[cfg(feature = "wasm_webrtc")]
+        // self.com_hub().register_sync_interface_factory::<crate::network::com_interfaces::webrtc_js_interface::WebRTCJSInterface>();
     }
 
     pub fn create_interface(
@@ -92,9 +96,7 @@ impl JSComHub {
                     )
                     .await
                     .map_err(|e| JsError::new(&format!("{e:?}")))?;
-                Ok(JsValue::from_str(
-                    &interface.borrow().get_uuid().0.to_string(),
-                ))
+                Ok(JsValue::from_str(&interface.to_string()))
             } else {
                 Err(JsError::new(
                     "Failed to create interface: properties are empty",
@@ -104,28 +106,24 @@ impl JSComHub {
         })
     }
 
-    pub fn close_interface(&self, interface_uuid: String) -> Promise {
+    pub fn close_interface(
+        &self,
+        interface_uuid: String,
+    ) -> Result<JsValue, JsError> {
         let interface_uuid =
             ComInterfaceUUID(UUID::from_string(interface_uuid));
         let runtime = self.runtime.clone();
-        future_to_promise(async move {
-            let com_hub = runtime.com_hub();
-            let has_interface = { com_hub.has_interface(&interface_uuid) };
-            if has_interface {
-                com_hub
-                    .remove_interface(interface_uuid.clone())
-                    .await
-                    .map_err(|e| JsError::new(&format!("{e:?}")))?;
-                Ok(JsValue::TRUE)
-            } else {
-                error!("Failed to find interface");
-                Err(JsError::new("Failed to find interface").into())
-            }
-        })
-    }
-
-    pub async fn update(&self) {
-        self.com_hub().update_async().await;
+        let com_hub = runtime.com_hub();
+        let has_interface = { com_hub.has_interface(&interface_uuid) };
+        if has_interface {
+            com_hub
+                .remove_interface(interface_uuid.clone())
+                .map_err(|e| JsError::new(&format!("{e:?}")))?;
+            Ok(JsValue::TRUE)
+        } else {
+            error!("Failed to find interface");
+            Err(JsError::new("Failed to find interface").into())
+        }
     }
 
     /// Send a block to the given interface and socket
@@ -134,58 +132,61 @@ impl JSComHub {
     /// The interface UUID is used to identify the interface to send the block over
     pub async fn send_block(
         &self,
-        block: &[u8],
+        block: Uint8Array,
         interface_uuid: String,
         socket_uuid: String,
-    ) -> bool {
+    ) -> Result<(), JsError> {
         let interface_uuid =
             ComInterfaceUUID(UUID::from_string(interface_uuid));
         let socket_uuid =
             ComInterfaceSocketUUID(UUID::from_string(socket_uuid));
-        self.com_hub()
-            .get_dyn_interface_by_uuid(&interface_uuid)
-            .expect("Failed to find interface")
-            .borrow_mut()
-            .send_block(block, socket_uuid)
+        let block = DXBBlock::from_bytes(block.to_vec().as_slice())
             .await
+            .map_err(|e| JsError::new(&format!("{e:?}")))?;
+        self.com_hub()
+            .interface_manager()
+            .borrow()
+            .get_interface_by_uuid(&interface_uuid)
+            .send_block(block, socket_uuid);
+        Ok(())
     }
 
-    pub fn _drain_incoming_blocks(&self) -> Vec<js_sys::Uint8Array> {
-        let mut sections = self
-            .com_hub()
-            .block_handler
-            .incoming_sections_queue
-            .borrow_mut();
-        let sections = sections.drain(..).collect::<Vec<_>>();
+    // pub fn _drain_incoming_blocks(&self) -> Vec<js_sys::Uint8Array> {
+    //     let mut sections = self
+    //         .com_hub()
+    //         .block_handler
+    //         .incoming_sections_queue
+    //         .borrow_mut();
+    //     let sections = sections.drain(..).collect::<Vec<_>>();
 
-        let mut blocks = vec![];
+    //     let mut blocks = vec![];
 
-        for section in sections {
-            match section {
-                IncomingSection::SingleBlock(block) => {
-                    blocks.push(block.clone());
-                }
-                _ => {
-                    panic!("Expected single block, but got block stream");
-                }
-            }
-        }
+    //     for section in sections {
+    //         match section {
+    //             IncomingSection::SingleBlock(block) => {
+    //                 blocks.push(block.clone());
+    //             }
+    //             _ => {
+    //                 panic!("Expected single block, but got block stream");
+    //             }
+    //         }
+    //     }
 
-        blocks
-            .iter()
-            .map(|(block, ..)| {
-                let bytes = block.clone().unwrap().to_bytes().unwrap();
-                let entry =
-                    js_sys::Uint8Array::new_with_length(bytes.len() as u32);
-                entry.copy_from(&bytes);
-                entry
-            })
-            .collect::<Vec<_>>()
-    }
+    //     blocks
+    //         .iter()
+    //         .map(|(block, ..)| {
+    //             let bytes = block.clone().unwrap().to_bytes().unwrap();
+    //             let entry =
+    //                 js_sys::Uint8Array::new_with_length(bytes.len() as u32);
+    //             entry.copy_from(&bytes);
+    //             entry
+    //         })
+    //         .collect::<Vec<_>>()
+    // }
 
     #[cfg(feature = "debug")]
     pub fn get_metadata_string(&self) -> String {
-        let metadata = self.com_hub().get_metadata();
+        let metadata = self.com_hub().metadata();
         metadata.to_string()
     }
 
@@ -208,13 +209,8 @@ impl JSComHub {
         self.com_hub().register_outgoing_block_interceptor(
             move |block, socket, endpoints| {
                 let this = JsValue::NULL;
-                let block_bytes = match block.to_bytes() {
-                    Ok(bytes) => js_sys::Uint8Array::from(&bytes[..]),
-                    Err(e) => {
-                        error!("Failed to convert block to bytes: {:?}", e);
-                        return;
-                    }
-                };
+                let block_bytes =
+                    js_sys::Uint8Array::from(block.to_bytes().as_slice());
                 let socket_uuid = JsValue::from_str(&socket.0.to_string());
                 let endpoints_array = js_sys::Array::new();
                 for endpoint in endpoints {
@@ -243,13 +239,8 @@ impl JSComHub {
         self.com_hub().register_incoming_block_interceptor(
             move |block, socket| {
                 let this = JsValue::NULL;
-                let block_bytes = match block.to_bytes() {
-                    Ok(bytes) => js_sys::Uint8Array::from(&bytes[..]),
-                    Err(e) => {
-                        error!("Failed to convert block to bytes: {:?}", e);
-                        return;
-                    }
-                };
+                let block_bytes =
+                    js_sys::Uint8Array::from(block.to_bytes().as_slice());
                 let socket_uuid = JsValue::from_str(&socket.0.to_string());
                 if let Err(e) = callback.call2(
                     &this,
