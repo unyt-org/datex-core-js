95c06cdd728ab1ed28ab1b905a4c2e4a5f06b74c
Jonas Strehle <jonas@strehles.info>
2026-07-18 17:42:26 +0200

    fix

--- test/runtime/sync.test.ts
+++ test/runtime/sync.test.ts
@@ -1,14 +1,16 @@
 import { Runtime } from "datex/runtime/runtime.ts";
 import { Endpoint } from "datex/lib/special-core-types/endpoint.ts";
-import {websocketServerDenoComInterfaceFactory} from "../../src/network/interfaces/websocket-server-deno.ts";
-import {sleep} from "../utils.ts";
+import { websocketServerDenoComInterfaceFactory } from "../../src/network/interfaces/websocket-server-deno.ts";
+import { sleep } from "../utils.ts";
 import { assertEquals } from "@std/assert/equals";
 import { assert } from "@std/assert/assert";
 import type { ReferencedSharedContainer } from "../../src/shared-container/mod.ts";
 
-async function getTwoConnectedRuntimes(): Promise<{runtimeA: Runtime, runtimeB: Runtime, cleanup: ()=>Promise<void>}> {
+async function getTwoConnectedRuntimes(): Promise<
+    { runtimeA: Runtime; runtimeB: Runtime; cleanup: () => Promise<void> }
+> {
     const PORT = 8099;
-    const runtimeA = await Runtime.create({ endpoint: Endpoint.get("@test_a") }, {log_level: "info"});
+    const runtimeA = await Runtime.create({ endpoint: Endpoint.get("@test_a") }, { log_level: "info" });
     runtimeA.comHub.registerInterfaceFactory(
         websocketServerDenoComInterfaceFactory,
     );
@@ -17,7 +19,7 @@ async function getTwoConnectedRuntimes(): Promise<{runtimeA: Runtime, runtimeB: 
         { bind_address: `0.0.0.0:${PORT}` },
     );
 
-    const runtimeB = await Runtime.create({ endpoint: Endpoint.get("@test_b") }, {log_level: "info"});
+    const runtimeB = await Runtime.create({ endpoint: Endpoint.get("@test_b") }, { log_level: "info" });
     const clientInterfaceUUID = await runtimeB.comHub.createInterface(
         "websocket-client",
         { url: `ws://localhost:${PORT}` },
@@ -31,19 +33,19 @@ async function getTwoConnectedRuntimes(): Promise<{runtimeA: Runtime, runtimeB: 
         cleanup: async () => {
             await runtimeA.comHub.removeInterface(serverInterfaceUUID);
             await runtimeB.comHub.removeInterface(clientInterfaceUUID);
-        }
-    }
+        },
+    };
 }
 
-
 Deno.test("sync value between two runtimes", async () => {
-    const {runtimeA, runtimeB, cleanup} = await getTwoConnectedRuntimes();
+    const { runtimeA, runtimeB, cleanup } = await getTwoConnectedRuntimes();
 
+    // b = {}; a = {a: b}; b.a = a
     await runtimeA.execute(`
         var x = shared mut 42; 
         @test_b.x = 'mut x;
-        @@local.x = 'mut x; 
-    `)
+        @@local.x = 'mut x;
+    `);
 
     const xOnA = runtimeA.executeSync<ReferencedSharedContainer<number>>("@@local.x");
     const xOnB = runtimeB.executeSync<ReferencedSharedContainer<number>>("@@local.x");
