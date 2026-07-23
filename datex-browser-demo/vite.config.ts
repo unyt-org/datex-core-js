dae17163b808bce381c281b0dd1f2e3791504924
Benedikt Strehle <benedikt@unyt.org>
2026-07-24 00:12:25 +0200

    fix browser demo

--- datex-browser-demo/vite.config.ts
+++ datex-browser-demo/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,4 +16,12 @@ export default defineConfig({
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
+    server: {
+        fs: {
+            allow: [
+                path.resolve(__dirname),
+                path.resolve(__dirname, "../npm/esm"),
+            ],
+        }
+    }
 });
